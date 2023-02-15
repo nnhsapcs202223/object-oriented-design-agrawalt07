199bbf3b4d57839a6e75a1f3e05a862e34d7a6c8
Tejas <tagrawal1@stu.naperville203.org>
2023-02-15 15:09:44 -0600

    t

--- ButtonViewer.java
+++ ButtonViewer.java
@@ -19,21 +19,26 @@ public class ButtonViewer
     private JPanel panel; 
     private JButton button; 
     private JLabel label; 
+    private JButton button2;
+    private int y; 
     private int x; 
     public ButtonViewer(){
         this.frame = new JFrame(); 
         this.x = 0; 
+        this.y = 0;
         this.panel = new JPanel(); 
         this.button = new JButton("Click me"); 
+        this.button2 = new JButton("No, Click me"); 
         this.label = new JLabel("0 clicks");
         this.panel.add(this.label);
         this.panel.add(this.button);
+        this.panel.add(this.button2); 
         this.frame.add(this.panel);
 
         ClickListener listener = new ClickListener();
         
         this.button.addActionListener(listener); 
-
+        this.button2.addActionListener(listener); 
         this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         this.frame.setVisible(true); 
@@ -46,8 +51,14 @@ public class ButtonViewer
     public class ClickListener implements ActionListener
     {
         public void actionPerformed(ActionEvent event){
-            x+=1; 
-            label.setText(x + "clicks"); 
+            if(event.getSource() == button){
+                x++; 
+            }
+            else if (event.getSource() == button2){
+                y++;
+            }
+   
+            label.setText(x + "clicks" + "// clicks: "+ y); 
         }
     }
 }
